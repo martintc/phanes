44b1631f3f8814895179860c54275411019f936d
Todd Martin <kj4ntv@gmail.com>
2022-04-09 16:48:54 -0700

    Add a new method for generating a new DB

--- src/datamanager/db.rs
+++ src/datamanager/db.rs
@@ -3,6 +3,20 @@ pub struct Database {
 }
 
 impl Database {
+    pub fn create_new_db(path: String) -> sqlite::Result<String, > {
+        let connection = sqlite::open(path)?;
+        let mut stmt = connection
+            .prepare("CREATE TABLE tasks(ID INTEGER PRIMARY KEY, TITLE TEXT, DESCRIPTION TEXT, STATUS INTEGER, CATEGORY INTEGER)")?;
+        stmt.next()?;
+        stmt = connection
+            .prepare("CREATE TABLE category(ID INTEGER PRIMARY KEY, NAME TEXT UNIQUE)")?;
+        stmt.next()?;
+        stmt = connection
+            .prepare("CREATE TABLE status(ID INTEGER KEY, STATUS TEXT)")?;
+        stmt.next()?;
+        Ok(String::from("Database  created"))
+    }
+
     pub fn new(path: String) -> Self {
         Database {
             path: path,
