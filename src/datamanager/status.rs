5a0b48c6d12c38031067e02a03c1a1334e044a02
Todd Martin <kj4ntv@gmail.com>
2022-04-16 20:52:53 -0700

    Add a get all status function

--- src/datamanager/status.rs
+++ src/datamanager/status.rs
@@ -1,6 +1,19 @@
-use sqlite;
+use sqlite::State;
+use std::vec::Vec;
 
 use crate::datamanager::db::Database;
+
+pub struct Status {
+	id: i64,
+	name: String,
+}
+
+impl Status {
+	pub fn print(&self) {
+		println!("ID: {}", self.id};
+		println!("\tName: {}", self.name);
+	}
+}
 
 pub fn get_status_name(db: &Database, id: i64) -> sqlite::Result<String> {
     let connection = sqlite::open(db.get_path())?;
@@ -16,4 +29,19 @@ pub fn get_status_id(db: &Database, name: String) -> sqlite::Result<i64> {
     stmt.bind(1, name.as_str())?;
     stmt.next()?;
     Ok(stmt.read::<i64>(0))?
+}
+
+pub fn get_all_status(db: &Database) -> sqlite::Result<Vec<Status>> {
+	let connection = sqlite::open(db.get_path()?;
+	let stmt = connection
+		.prepare("select ID, NAME from status")?;
+	let mut result: Vec<Status> = Vec::new();
+	while let State::Row = stmt.next()? {
+		let s = Status {
+			id: stmt.read::<i64>(0)?,
+			name: stmt.read::<String>(1)?,
+		};
+		result.push(s);
+	}
+	Ok(result)
 }
