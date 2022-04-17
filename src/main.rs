95fdde14d70efcfe57ad4d9f97056ad045b18ead
Todd Martin <kj4ntv@gmail.com>
2022-04-17 03:17:42 -0700

    WIP: Have it launch the new TUI that is WIP

--- src/main.rs
+++ src/main.rs
@@ -1,9 +1,13 @@
 use menu::Menu;
+use tui::*;
 
 mod datamanager;
 mod menu;
+mod tui;
 
 fn main() {
-    let menu = Menu::new();
-    menu.run();
+    // let menu = Menu::new();
+    // menu.run();
+    let tui: TUI_Instance;
+    tui.run_app();
 }
