85b71a5a9438d6d40e11f20237f290bb1b12f38b
Todd Martin <33560895+martintc@users.noreply.github.com>
2022-04-16 21:15:57 -0700

    Update Main to dev (#5)
    * Implement a task structure (#1)
    
    * Add Task structure
    
    Add a structure for Task and implement some methods. One method is a print
    method for Task. The other method searches a DB for the task by ID.
    
    * Add option to get info on a task given a task ID
    
    * Implement Category structure and more database fixes (#2)
    
    * Add Task structure
    
    Add a structure for Task and implement some methods. One method is a print
    method for Task. The other method searches a DB for the task by ID.
    
    * Add option to get info on a task given a task ID
    
    * Implement a category structure
    
    Implement a print function for a function
    
    Implement returning a vector of Category instead of a vector of Strings
    
    * Inserting in default values into a newly created database
    
    * Implement printing all categories using a vector of categories inste
    
    of strings. Also panic with an error message instead of panicing without a message
    
    * Implement more structures (#4)
    
    * Add Task structure
    
    Add a structure for Task and implement some methods. One method is a print
    method for Task. The other method searches a DB for the task by ID.
    
    * Add option to get info on a task given a task ID
    
    * Implement a category structure
    
    Implement a print function for a function
    
    Implement returning a vector of Category instead of a vector of Strings
    
    * Inserting in default values into a newly created database
    
    * Implement printing all categories using a vector of categories inste
    
    of strings. Also panic with an error message instead of panicing without a message
    
    * Format fixing
    
    * Add a get all status function
    
    * Fixes for syntax not picked up when testing zee text editor
    
    * No longer need testing file
    
    * Simplify SQL stataments for creating database
    
    * Call self.name instead of self.id to print correct value

--- src/datamanager/db.rs
+++ src/datamanager/db.rs
@@ -2,13 +2,9 @@ pub struct Database {
     path: String,
 }
 
-
-
 impl Database {
-    pub fn create_new_db(path: String) -> sqlite::Result<Database, > {
-        let db = Database {
-            path: path
-        };
+    pub fn create_new_db(path: String) -> sqlite::Result<Database> {
+        let db = Database { path: path };
         let connection = sqlite::open(&db.path)?;
         let mut stmt = connection
             .prepare("CREATE TABLE tasks(ID INTEGER PRIMARY KEY, TITLE TEXT, DESCRIPTION TEXT, STATUS INTEGER, CATEGORY INTEGER);")?;
@@ -16,8 +12,27 @@ impl Database {
         stmt = connection
             .prepare("CREATE TABLE category(ID INTEGER PRIMARY KEY, NAME TEXT UNIQUE);")?;
         stmt.next()?;
+        stmt = connection.prepare("CREATE TABLE status(ID INTEGER KEY, STATUS TEXT);")?;
+        stmt.next()?;
+        stmt = connection.prepare("insert into status(STATUS) values('open')")?;
+        stmt.next()?;
+        stmt = connection.prepare("insert into status(STATUS) values('in-progress')")?;
+        stmt.next()?;
+        stmt = connection.prepare("insert into status(STATUS) values('closed')")?;
+        stmt.next()?;
+        stmt = connection.prepare("insert into category(NAME) values('none')")?;
+        stmt.next()?;
         stmt = connection
-            .prepare("CREATE TABLE status(ID INTEGER KEY, STATUS TEXT);")?;
+            .prepare("insert into status values(1, 'open')")?;
+        stmt.next()?;
+        stmt = connection
+            .prepare("insert into status values(2, 'in-progress')")?;
+        stmt.next()?;
+        stmt = connection
+            .prepare("insert into status values(3, 'closed')")?;
+        stmt.next()?;
+        stmt = connection
+            .prepare("insert into category values(1, 'none')")?;
         stmt.next()?;
         stmt = connection
             .prepare("insert into status values(1, 'open')")?;
@@ -35,9 +50,7 @@ impl Database {
     }
 
     pub fn new(path: String) -> Self {
-        Database {
-            path: path,
-        }
+        Database { path: path }
     }
 
     pub fn get_path(&self) -> &str {
--- src/datamanager/mod.rs
+++ src/datamanager/mod.rs
@@ -1,4 +1,4 @@
-pub mod task;
-pub mod status;
 pub mod category;
 pub mod db;
+pub mod status;
+pub mod task;
